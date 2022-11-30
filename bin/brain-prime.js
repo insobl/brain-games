11e74bbacb2e4d045fadc8b5cd38a84aaff1b983
insobl <kapustin.daniil@hexly.ru>
2022-11-30 12:59:25 +0300

    add bin/brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import {userName} from '../src/cli.js';
+import {checkAnswer, randomizer, congratulation, isItPrime} from '../src/index.js';
+var name =  userName();
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+for(var i = 1; i<=3; i++){
+var number = randomizer(100)+1,
+    correctAnswer= isItPrime(number),
+    userAnswer = readlineSync.question('Your answer: '),
+    result = checkAnswer(correctAnswer, userAnswer, name);
+    if (result === 'stop, wrong answer') {
+    break;
+}
+congratulation(name,i);
+}
