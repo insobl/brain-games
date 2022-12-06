f64500734577c871159cf816a470ca2b4fd0ce32
insobl <dan_kapustin@mail.ru>
2022-12-06 13:11:05 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 export function userName(){
 console.log('Welcome to the Brain Games!');
-var name = readlineSync.question('May I Have your name? ');
+const name = readlineSync.question('May I Have your name? ');
 return name;
 }
