1a1bd666efd55b028005b07ed9eb2d7cb9c532d8
insobl <dan_kapustin@mail.ru>
2022-12-06 13:03:08 +0300

    move games

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import {userName} from '../../src/cli.js';
-import {checkAnswer, randomizer, congratulation} from '../../src/index.js';
+import {userName} from '../src/cli.js';
+import {checkAnswer, randomizer, congratulation} from '../src/index.js';
 import readlineSync from 'readline-sync';
 var name =  userName();
 console.log(`Hello, ${name}!`);
